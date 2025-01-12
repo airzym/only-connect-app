39c6480356dadc823450d47b8bcdc5853aa8dbf2
Aaron Bamford <aaron.bamford@icloud.com>
2025-01-12 17:31:54 +0000

    Start implementing slow on connections to capture outcome

--- OnlyConnect.App/ClientApp/src/components/Connection.js
+++ OnlyConnect.App/ClientApp/src/components/Connection.js
@@ -1,19 +1,24 @@
 import React, {useEffect, useState} from 'react';
-import {Breadcrumb, BreadcrumbItem, Button, Col, Row} from "reactstrap";
+import { Button, Col, Row} from "reactstrap";
 import {Clue} from "./clue/Clue";
 import {Answer} from "./answer/Answer";
+import {TeamName} from "./teamName/TeamName";
 
 export function Connection(props) {
-    const [state, setState] = useState({ connection: null, loading: true, cluesGiven: null, answerRevealed: false });
+    const [game, setGame] = useState(null);
+    const [leading, setLoading] = useState(true);
+    const [teamBuzzed, setTeamBuzzed] = useState(false);
+    const [state, setState] = useState({ cluesGiven: null, answerRevealed: false });
 
     useEffect(() => {
         async function fetchData() {
-            const response = await fetch(`api/game/connections/${props.icon}`);
+            const response = await fetch(`api/game`);
             const data = await response.json();
-            setState({connection: data, loading: false, cluesGiven: 0, answerRevealed: false});
+            setGame(data);
+            setLoading(false)
         }
         
-        if(state.connection === null){
+        if(game === null){
             fetchData();
         }
         
@@ -23,31 +28,54 @@ export function Connection(props) {
         setState({ ...state,  cluesGiven: state.cluesGiven + 1 });
     }
 
+    function buzzTeam() {
+        setTeamBuzzed(true);
+    }
+
+    function throwToOpponent() {
+        
+    }
+
     function revealAnswer() {
         setState({ ...state,  answerRevealed: true });
+    }
+    
+    function getConnection(){
+        return game.connections.find(x => x.name === props.icon);
     }
 
     function renderForecastsTable() {
         return (
             <div>
-                <Row className="my-3">
-                    <Col><Clue clue={state.connection.clueOne} show={state.cluesGiven >= 1 || state.answerRevealed} current={state.cluesGiven === 1} points="5"></Clue></Col>
-                    <Col><Clue clue={state.connection.clueTwo} show={state.cluesGiven >= 2 || state.answerRevealed} current={state.cluesGiven === 2} points="3"></Clue></Col>
-                    <Col><Clue clue={state.connection.clueThree} show={state.cluesGiven >= 3 || state.answerRevealed} current={state.cluesGiven === 3} points="2"></Clue></Col>
-                    <Col><Clue clue={state.connection.clueFour} show={state.cluesGiven >= 4 || state.answerRevealed} current={state.cluesGiven === 4} points="1"></Clue></Col>
+                <Row>
+                    <Col md={6} className="offset-3">
+                        <TeamName name={game.currentTeam.teamName}></TeamName>
+                    </Col>
                 </Row>
                 <Row className="my-3">
-                    {state.answerRevealed && <Col><Answer answer={state.connection.answer}></Answer></Col> }
+                    <Col><Clue clue={getConnection().clueOne} show={state.cluesGiven >= 1 || state.answerRevealed} current={state.cluesGiven === 1} points="5"></Clue></Col>
+                    <Col><Clue clue={getConnection().clueTwo} show={state.cluesGiven >= 2 || state.answerRevealed} current={state.cluesGiven === 2} points="3"></Clue></Col>
+                    <Col><Clue clue={getConnection().clueThree} show={state.cluesGiven >= 3 || state.answerRevealed} current={state.cluesGiven === 3} points="2"></Clue></Col>
+                    <Col><Clue clue={getConnection().clueFour} show={state.cluesGiven >= 4 || state.answerRevealed} current={state.cluesGiven === 4} points="1"></Clue></Col>
+                </Row>
+                <Row className="my-3">
+                    {state.answerRevealed && <Col><Answer answer={getConnection().answer}></Answer></Col> }
                 </Row>
                 <Row className="my-3">
                     <Col>
-                        <Button color="success" onClick={revealAnswer} disabled={state.cluesGiven < 1}>
-                            Reveal Answer
+                        <Button color="success" onClick={buzzTeam} disabled={state.cluesGiven < 1} hidden={teamBuzzed}>
+                            Buzz
+                        </Button>
+                        <Button color="danger" onClick={throwToOpponent} disabled={state.cluesGiven < 1} hidden={!teamBuzzed}>
+                            Incorrect
                         </Button>
                         <div className="float-end">
-                            <Button color="primary" onClick={next} disabled={state.cluesGiven === 4 || state.answerRevealed}>
+                            <Button color="primary" onClick={next} disabled={state.cluesGiven === 4 || state.answerRevealed} hidden={teamBuzzed}>
                                 Next
-                            </Button>    
+                            </Button>
+                            <Button color="success" onClick={revealAnswer} disabled={state.cluesGiven < 1} hidden={!teamBuzzed}>
+                                Correct
+                            </Button>
                         </div>
                     </Col>
                 </Row>
@@ -55,22 +83,12 @@ export function Connection(props) {
         );
     }
 
-    let contents = state.loading
+    let contents = leading
         ? <p><em>Loading...</em></p>
         : renderForecastsTable();
 
     return (
         <div>
-            <Breadcrumb>
-                <BreadcrumbItem>
-                    <a href="/connections">
-                        Connections
-                    </a>
-                </BreadcrumbItem>
-                <BreadcrumbItem active>
-                    {props.icon}
-                </BreadcrumbItem>
-            </Breadcrumb>
             <h1 id="tableLabel">Connections</h1>
             <p>What connects all four clues?</p>
             {contents}
